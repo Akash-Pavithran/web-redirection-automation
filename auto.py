65d23579db24444866e5afc4f0e5af6f227ac024
Akash <61753841+AkashDeVolvo@users.noreply.github.com>
2020-11-28 17:57:55 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,4 @@
 import webbrowser
-url = ['https://www.facebook.com','https://www.unsplash.com','https://www.pixabay.com']
+url = ['https://www.pexels.com','https://www.unsplash.com','https://www.pixabay.com']
 for web in url:
     webbrowser.open(web)
